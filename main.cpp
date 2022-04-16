4ce621eca48582ccb5130ec94b3661a7a30b6069
osama maher <osama392maher@gmail.com>
2022-04-16 07:32:08 +0200

    detect filter first edition

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 // FCI – Programming 1 – 2022 - Assignment 3
 // Program Name: filters.cpp // Last Modification Date: 5/4/2022
-￼    // Author1 and ID : Osama Maher Masoued    20210060
+      // Author1 and ID : Osama Maher Masoued    20210060
      // Author2 and ID : Nada Essam El_Din Essa 20211104
      // Author3 and ID : Sara Adel Hanafy Bayoumi 20211046
      // Group : S1 - S2
@@ -30,6 +30,7 @@ void flip_image();
 void rotate_image();
 void darken_and_lighten_image();
 void mirror_image();
+void detect_edges();
 
 int main()
 {
@@ -44,6 +45,7 @@ int main()
   cout <<"    5- Rotate Image" << endl;
   cout <<"    6- Darken and Lighten Image" << endl;
   cout <<"    7- mirror " << endl;
+  cout <<"    8- detect image " << endl;
   cout <<"    0- exit" << endl;
 
   cin >> choice;
@@ -89,6 +91,11 @@ int main()
     loadImage();
     mirror_image();
     saveImage();
+  }
+  else if (choice == 8){
+    loadImage();
+    detect_edges();
+    save_newImage();
   }
   else if (choice == 0){
     return 0;
@@ -172,6 +179,7 @@ void black_and_white() {
     }
 }
 
+//_________________________________________
 void invert_image() {
 
     for (int i = 0; i < SIZE; i++) {
@@ -182,6 +190,7 @@ void invert_image() {
     }
 }
 
+//_________________________________________
 void merge_images() {
 
     for (int i = 0; i < SIZE; i++) {
@@ -191,6 +200,7 @@ void merge_images() {
     }
 }
 
+//_________________________________________
 void flip_image() {
   int choice;
   cout << "How do you want to flip the image? 😌" << endl;
@@ -219,6 +229,7 @@ void flip_image() {
   }
 }
 
+//_________________________________________
 void rotate_image() {
 
     int choice;
@@ -287,6 +298,7 @@ void rotate_image() {
 
 }
 
+//_________________________________________
 void darken_and_lighten_image() {
 
   int choice;
@@ -316,6 +328,8 @@ void darken_and_lighten_image() {
 	}
 
 }
+
+//_________________________________________
 void mirror_image() {
 
     int choice;
@@ -365,5 +379,52 @@ void mirror_image() {
   }
 
   
+
+}
+
+//_________________________________________
+
+void detect_edges() {
+
+int gx_val[3][3] = {{-1, 0, 1},
+                    {-2, 0, 2},
+                    {-1, 0, 1}};
+
+int gy_val[3][3] = {{-1,-2,-1},
+                    {0, 0, 0},
+                    {1, 2, 1}};
+
+double gx, gy;
+int i_index = 0;
+int j_index = 0;
+int pixel_val;
+
+for (int i = 0; i < SIZE; i++){
+    for (int j = 0; j < SIZE; j++){
+        gx = 0;
+        gy = 0;
+
+        for (int x = 0; x < 3; x++){
+            for (int y = 0; y < 3; y++){
+
+                i_index = (i - 1) + x;
+                j_index = (j - 1) + y;
+
+                if (0 <= i_index && i_index <= 255 && 0 <= j_index && j_index <= 255){
+                    gx += image[i_index][j_index] * gx_val[x][y];
+                    gy += image[i_index][j_index] * gy_val[x][y];
+                }
+            }
+        }
+
+        pixel_val = round(sqrt((gx * gx) + (gy * gy)));
+
+        if (pixel_val > 255) {
+          pixel_val = 255;
+        }
+        
+        new_image[i][j] = pixel_val;
+    }
+}
 
 }
